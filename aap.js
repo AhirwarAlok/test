1bba69ff1ecc5879f3cc999cb7b805ab538859b3
AhirwarAlok <54554233+AhirwarAlok@users.noreply.github.com>
2023-06-14 14:00:42 +0530

    Update aap.js

--- aap.js
+++ aap.js
@@ -1,112 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  Link,
-} from "react-router-dom";
-import Login from "./component/Login";
-import Admin from "./component/Admin";
-import Dashbord from "./component/Dashbord";
-import Help from "./component/Help";
-
-const App = () => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
-
-  const PrivateRoute = ({ element: Element, roles }) => {
-    const userRole = loggedInUser?.role;
-
-    console.log(userRole);
-    if (!loggedInUser || (roles && !roles.includes(userRole))) {
-      return <Navigate to="/login" />;
-    }
-
-    return Element;
-  };
-  const handleLogout = () => {
-    setLoggedInUser(null);
-  };
-
-  const NavBar = () => {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="navbar-brand">LAB5 </div>
-        <ul className="navbar-nav">
-          {loggedInUser && (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/help">
-                  Help
-                </Link>
-              </li>
-            </>
-          )}
-          {loggedInUser && loggedInUser.role === "Admin" && (
-            <li className="nav-item">
-              <Link className="nav-link" to="/admin">
-                Admin Page
-              </Link>
-            </li>
-          )}
-          {loggedInUser && (
-            <li>
-              <p class="text-danger p">
-                You're logged in as {loggedInUser.role}
-              </p>
-            </li>
-          )}
-          {!loggedInUser && (
-            <li>
-              <p class="text-danger p  fs-5">Login to access the application</p>
-            </li>
-          )}
-        </ul>
-        {loggedInUser && <button className="btn btn-danger" onClick={handleLogout}> logout</button>}
-      </nav>
-    );
-  };
-
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={<Login setLoggedInUser={setLoggedInUser} />}
-        ></Route>
-        <Route
-          path="/login"
-          element={<Login setLoggedInUser={setLoggedInUser} />}
-        ></Route>
-        <Route
-          path="/admin"
-          element={
-            <PrivateRoute path="/admin" element={<Admin />} roles={["Admin"]} />
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute element={<Dashbord />} roles={["Admin", "User"]} />
-          }
-        />
-        <Route
-          path="/help"
-          element={
-            <PrivateRoute element={<Help />} roles={["Admin", "User"]} />
-          }
-        />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
+{
+  "name": "card_modal",
+  "version": "0.1.0",
+  "private": true,
+  "dependencies": {
+    "@emotion/react": "^11.11.1",
+    "@emotion/styled": "^11.11.0",
+    "@mui/material": "^5.13.5",
+    "@testing-library/jest-dom": "^5.16.5",
+    "@testing-library/react": "^13.4.0",
+    "@testing-library/user-event": "^13.5.0",
+    "axios": "^1.4.0",
+    "bootstrap": "^5.3.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-icons": "^4.9.0",
+    "react-router-dom": "^6.12.1",
+    "react-scripts": "5.0.1",
+    "web-vitals": "^2.1.4"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "eslintConfig": {
+    "extends": [
+      "react-app",
+      "react-app/jest"
+    ]
+  },
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  }
+}
